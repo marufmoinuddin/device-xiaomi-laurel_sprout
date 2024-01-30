b9409a76496203c8e494072e56da7ccce42411a0
aleeeee1 <pace.alessandro05@protonmail.com>
2024-01-30 11:44:57 +0000

    laurel_sprout: device.mk: build dlopen

--- device.mk
+++ device.mk
@@ -115,6 +115,10 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+# Debug
+PRODUCT_PACKAGES += \
+    dlopen
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-impl \
