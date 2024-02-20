d190ec05d6c8493e00af6923fb4fe79b44ed6a24
unsatifsed27 <unsatifsederror@gmail.com>
2024-02-20 21:55:18 +0100

    laurel_sprout: built libbthost_if_sink

--- device.mk
+++ device.mk
@@ -78,7 +78,8 @@ PRODUCT_PACKAGES += \
     libldacBT_bco \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    libbthost_if_sink
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
