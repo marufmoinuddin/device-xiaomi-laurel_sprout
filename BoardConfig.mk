e99edba3893011d3fac9387be827a9aced10f889
aleeeee1 <pace.alessandro05@protonmail.com>
2024-03-02 09:47:14 +0000

    Revert "laurel_sprout: made selinux permissive"
    This reverts commit 06140b66b816c2898ec36637b4ef00f2ad1b6632.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,7 +102,6 @@ BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += cgroup.memory=nokmem,nosocket
 BOARD_KERNEL_CMDLINE += buildvariant=user
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 TARGET_KERNEL_CONFIG := vendor/laurel_sprout-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/laurel_sprout
