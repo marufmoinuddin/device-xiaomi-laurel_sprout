060219b68052bfece462957a68e637faf80f5be2
aleeeee1 <pace.alessandro05@protonmail.com>
2024-03-26 13:59:07 +0000

    laurel_sprout: compile kernel with proton

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -107,7 +107,7 @@ TARGET_KERNEL_CONFIG := vendor/laurel_sprout-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/laurel_sprout
 
 TARGET_KERNEL_NO_LLVM_BINUTILS := true
-TARGET_KERNEL_CLANG_PATH := ~/toolchains/neutron-clang
+TARGET_KERNEL_CLANG_PATH := ~/toolchains/proton-clang
 KERNEL_LD := LD=ld.lld
 
 # Lights
