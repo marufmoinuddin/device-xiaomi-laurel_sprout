9c0344792d18b147ff9311ea4179e6b8a7d3a5ed
aleeeee1 <pace.alessandro05@protonmail.com>
2024-05-15 20:13:46 +0200

    Revert "laurel_sprout: Enable updating of APEXes"
    Now this is default in QPR2
    
    This reverts commit a6e8842d691116632caefdc178502b3dc89cb012.

--- device.mk
+++ device.mk
@@ -244,9 +244,6 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
