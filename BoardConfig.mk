88ca4e7418e9766c89efe40dcf8b8a58af74d870
aleeeee1 <pace.alessandro05@protonmail.com>
2024-06-21 11:38:21 +0000

    laurel_sprout: moved manifests to manifest's folder

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,8 +78,8 @@ BOARD_HAS_QCA_FM_SOC := "cherokee"
 BOARD_HAVE_QCOM_FM := true
 
 # HIDL
-DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/manifests/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/manifests/manifest.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_laurel_sprout
